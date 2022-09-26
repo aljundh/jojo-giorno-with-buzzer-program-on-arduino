d7df961326e40f9f84ebe0cd5413569e26aa27bc
Hafis Royhan <107126204+aljundh@users.noreply.github.com>
2022-09-26 15:39:55 +0700

    Add files via upload

---
+++ jojo.ino
@@ -0,0 +1,50 @@
+ void setup()
+{
+  pinMode(13, OUTPUT);
+}
+
+void loop()
+{
+  tone(13, 740, 1000); // play tone 66 (F#5 = 740 Hz)
+  delay(500);
+  tone(13, 587, 1000); // play tone 62 (D5 = 587 Hz)
+  delay(500);
+  tone(13, 587, 1000); // play tone 62 (D5 = 587 Hz)
+  delay(400);
+  tone(13, 659, 1000); // play tone 64 (E5 = 659 Hz)
+  delay(300);
+  tone(13, 698, 1000); // play tone 65 (F5 = 698 Hz)
+  delay(300); 
+  tone(13, 659, 1000); // play tone 64 (E5 = 659 Hz)
+  delay(300);
+  tone(13, 587, 1000); // play tone 62 (D5 = 587 Hz)
+  delay(300);
+  tone(13, 622, 1000); // play tone 63 (D#5 = 622 Hz)
+  delay(300);
+  tone(13, 587, 1000); // play tone 62 (D5 = 587 Hz)
+  delay(300);
+  tone(13, 659, 1000); // play tone 64 (E5 = 659 Hz)
+  delay(300);
+  tone(13, 740, 1000); // play tone 66 (F#5 = 740 Hz)
+  delay(300);
+  tone(13, 988, 1000); // play tone 71 (B5 = 988 Hz)
+  delay(300);
+  tone(13, 988, 1000); // play tone 71 (B5 = 988 Hz)
+  delay(300);
+  tone(13, 554, 1000); // play tone 61 (C#5 = 554 Hz)
+  delay(300);
+  tone(13, 587, 1000); // play tone 62 (D5 = 587 Hz)
+  delay(300);
+  tone(13, 659, 1000); // play tone 64 (E5 = 659 Hz)
+  delay(300);
+  tone(13, 587, 1000); // play tone 62 (D5 = 587 Hz)
+  delay(300);
+  tone(13, 554, 1000); // play tone 61 (C#5 = 554 Hz)
+  delay(300);
+  tone(13, 880, 1000); // play tone 69 (A5 = 880 Hz)
+  delay(300);
+  tone(13, 784, 1000); // play tone 67 (G5 = 784 Hz)
+  delay(300);
+  tone(13, 740, 1000); // play tone 66 (F#5 = 740 Hz)
+  delay(300);
+}
